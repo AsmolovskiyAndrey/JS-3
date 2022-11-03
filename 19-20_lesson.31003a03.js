1db7327003541b164d64f21149889546111a36bd
AsmolovskiyAndrey <105419171+AsmolovskiyAndrey@users.noreply.github.com>
2022-11-03 14:44:30 +0000

    Deploying to gh-pages from @ AsmolovskiyAndrey/JS-3@b0cc3eb73271397ba186f08746d99cfa5f7d0359 🚀

---
+++ 19-20_lesson.31003a03.js
@@ -0,0 +1,3 @@
+//! ============  Протокол HTTP =========================
+fetch("https://restcountries.com/v3.1/all").then((t=>t.json())).then((t=>console.log(t))),fetch("https://rickandmortyapi.com/api/character").then((t=>t.json())).then((t=>console.log(t)));
+//# sourceMappingURL=19-20_lesson.31003a03.js.map
---
+++ 19-20_lesson.da4d1f5c.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/all").then((function(n){return n.json()})).then((function(n){return console.log(n)})),fetch("https://rickandmortyapi.com/api/character").then((function(n){return n.json()})).then((function(n){return console.log(n)}));
+//# sourceMappingURL=19-20_lesson.da4d1f5c.js.map
