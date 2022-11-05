6166ce0579added85dba1d783d37bf1829fb0147
AsmolovskiyAndrey <105419171+AsmolovskiyAndrey@users.noreply.github.com>
2022-11-05 11:49:00 +0000

    Deploying to gh-pages from @ AsmolovskiyAndrey/JS-3@4ee51daa60469eb7e7ea5def107c31a1d198d6e5 🚀

--- 19-20_lesson.922185cf.js
+++
@@ -1,2 +0,0 @@
-fetch("https://pokeapi.co/api/v2/pokemon").then((function(n){return n.json()})).then((function(n){return console.log(n)}));
-//# sourceMappingURL=19-20_lesson.922185cf.js.map
--- 19-20_lesson.be409c06.js
+++
@@ -1,3 +0,0 @@
-//! ============  Протокол HTTP =========================
-fetch("https://pokeapi.co/api/v2/pokemon").then((o=>o.json())).then((o=>console.log(o)));
-//# sourceMappingURL=19-20_lesson.be409c06.js.map
